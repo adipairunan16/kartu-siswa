3f96dbe3e060079bd06b90d6028017674befe92b
Adi Pairunan <adi.pairunan16@guru.smk.belajar.id>
2026-07-19 09:10:57 +0800

    lren

---
+++ app/api/test-folder/route.js
@@ -0,0 +1,25 @@
+import { google } from "googleapis";
+
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+});
+
+const drive = google.drive({
+  version: "v3",
+  auth,
+});
+
+export async function GET() {
+  const response = await drive.files.list({
+    q: `'${process.env.GOOGLE_DRIVE_FOLDER_ID}' in parents and trashed=false`,
+    fields: "files(id,name,mimeType)",
+    pageSize: 200,
+  });
+
+  return Response.json(response.data.files);
+}
